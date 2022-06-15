47a00ab0e02ee351639988ea723541b77b7e9a9b
ssun-aa <ssk6200@daum.net>
2022-06-15 18:26:14 +0900

    file: svg files remove and add

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,4 +1,4 @@
 export { ReactComponent as SearchIcon } from './search.svg'
 export { ReactComponent as StarIcon } from './star.svg'
 export { ReactComponent as ColorStarIcon } from './colorStar.svg'
-export { ReactComponent as CancelIcon } from './cancel.svg'
+export { ReactComponent as CheckIcon } from './check.svg'
